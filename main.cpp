e8635f77ce4782a8510c747d3d9d30bead1509c4
TAnon <vasyuk.a.a@yandex.ru>
2026-05-19 01:03:24 +0300

    Added base classes and some functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,63 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <math.h>
+
+
+#define G 10
 
 
 class Object
 {
-    private
+private:
+    int x;
+    int y;
+    int vx;
+    int vy;
+    int mass;
+
+public:
+    Object(int x, int y, int vx, int vy)
+    {
+        this->x = x;
+        this->y = y;
+        this->vx = vx;
+        this->vy = vy;
+    }
+
+    std::pair<int, int> get_coordinates() { return std::pair<int, int>(x, y); }
+
+    int get_mass() { return this->mass; }
+
+    void add_velocity(int dvx=0, int dvy=0)
+    {
+        vx += dvx;
+        vy += dvy;
+    }
+
+    void add_velocity(Object &other, float dv = 0)
+    {
+
+
+        vx += dvx;
+        vy += dvy;
+    }
+
+};
+
+
+float distance_between_objects(Object a, Object b);
+
+
+class Planet: public Object
+{
+private:
+    int radius;
+    
+public:
+    void Attract_to_object(Object &other)
+    {
+        float dv = (G * other.get_mass()) / std::pow(distance_between_objects(*this, other), 2);
+        this->add_velocity(other, dv);
+    }
 };
 
 
@@ -21,4 +75,11 @@ int main()
                 window.close();
         }
     }
+}
+
+
+float distance_between_objects(Object a, Object b)
+{
+    auto xa = a.get_coordinates(), xb = b.get_coordinates();
+    return std::sqrt(std::pow(xa.first - xb.first, 2) + std::pow(xa.second - xb.second, 2));
 }
