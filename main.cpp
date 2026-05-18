d94f78e2e33665d8aa8e367fdc5ea6f175abdd2a
vasyu <vasyu@RICKROLLJRMARK2>
2026-05-18 19:56:02 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+
+
+class Object
+{
+    private
+};
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "First time", sf::State::Fullscreen);
+
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
+}
