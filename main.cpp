705a9d4dab24e0da8c5ab007611abfb2fa4520cb
TAnon <vasyuk.a.a@yandex.ru>
2026-05-22 02:32:11 +0300

    Planets go spiiin. Actually workds with real planets (Earth and Moon) parameteressss

--- main.cpp
+++ main.cpp
@@ -10,8 +10,9 @@
 //float G = 6.67430 * std::pow(10, -11);
 //int WARP = 4 * 10000;
 
-float G = 10000;
-int WARP = 1;
+float G = 6.67430;
+int WARP = 100000;
+int SCALE = 1e7 / 4;
 
 
 class Object
@@ -27,6 +28,7 @@ public:
     Object(float x, float y, float vx, float vy, int mass);
     std::pair<float, float> get_coordinates();
     int get_mass();
+    void attract_to_object(Object& other);
     void add_velocity(float dvx = 0, float dvy = 0);
     void add_velocity(Object& other, float dv = 0);
     void update();
@@ -68,6 +70,13 @@ void Object::add_velocity(Object& other, float dv)
     vy += dv * sin;
 }
 
+void Object::attract_to_object(Object& other)
+{
+    float dv = (G * other.get_mass()) * 100000 / std::pow(distance_between_objects(*this, other), 2);
+    this->add_velocity(other, dv * WARP);
+    //std::cout << dv * WARP << '\n';
+}
+
 void Object::update()
 {
     this->x += this->vx * WARP;
@@ -92,25 +101,22 @@ public:
         this->color = color;
     }
 
-    void Attract_to_object(Object& other)
-    {
-        float dv = (G * other.get_mass()) / std::pow(distance_between_objects(*this, other), 2);
-        this->add_velocity(other, dv * WARP);
-    }
-
     std::tuple<short, short, short> get_color() { return this->color; }
 
-    void draw(sf::RenderWindow& window)
+    void draw(sf::RenderWindow& window, int scale, float x, float y)
     {
-        sf::CircleShape circle(radius);
+        sf::CircleShape circle(radius / scale + 10);
 
         auto c = this->get_coordinates();
-        circle.setPosition({ (float)c.first, (float)c.second });
+        circle.setPosition({ ((float)c.first) / scale + x, ((float)c.second) / scale + y });
+
+        //std::cout << radius / scale << '\n';
 
         auto color = this->get_color();
 
         circle.setFillColor(sf::Color(std::get<0>(color), std::get<1>(color), std::get<2>(color)));
         //circle.setFillColor(sf::Color(255, 255, 255));
+        //sf::Color()
 
         //std::cout << '1';
 
@@ -126,10 +132,10 @@ int main()
     window.setFramerateLimit(24);
 
     std::tuple<short, short, short> c1, c2;
-    c1 = std::make_tuple(255, 255, 255);
-    c2 = std::make_tuple(0, 255, 255);
+    c1 = std::make_tuple(50, 70, 219);
+    c2 = std::make_tuple(217, 213, 206);
 
-    Planet a(WIDTH / 2, HEIGHT / 2, 0, 0, 1, 20, c1), b(WIDTH / 2 + 200, HEIGHT / 2, 0, -5, 1, 10, c2);
+    Planet a(0, 0, 0, 0, 5.972e8, 6378e3, c1), b(384399e3, 0, 0, -1022, 7.346e6, 1737e3, c2);
 
     while (window.isOpen())
     {
@@ -143,13 +149,13 @@ int main()
 
         window.clear();
 
-        b.Attract_to_object(a);
+        b.attract_to_object(a);
 
         a.update();
         b.update();
 
-        a.draw(window);
-        b.draw(window);
+        a.draw(window, SCALE, WIDTH / 2, HEIGHT / 2);
+        b.draw(window, SCALE, WIDTH / 2, HEIGHT / 2);
 
         window.display();
 
